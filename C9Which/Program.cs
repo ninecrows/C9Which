c205367a92d93649f3087f771c5abac713a74bf7
Kyle Wilson <kyle@ninecrows.com>
2018-02-25 23:02:01 -0500

    Initial version. Needs better command line handling

---
+++ C9Which/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C9Which
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetEnvironmentVariable("path");
+
+            string[] targets = path.Split(';');
+
+            if (args.Length > 0)
+            {
+                string askAbout = args[0].ToLower();
+                for (int ct = 0; ct < targets.Length; ct++)
+                {
+                    string thisTarget = targets[ct];
+                    if (Directory.Exists(thisTarget))
+                    {
+                        string[] files = Directory.GetFiles(thisTarget);
+
+                        foreach (string file in files)
+                        {
+                            string foldFileName = file.ToLower();
+                            string justFileName = Path.GetFileName(foldFileName);
+                            if (justFileName.Equals(args[0]))
+                            {
+                                Console.Out.WriteLine(file);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine($"Not a folder \"{thisTarget}\"");
+                    }
+                }
+            }
+        }
+    }
+}
