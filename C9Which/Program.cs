e4584666d433987774276a0e6cf84a74ef517baf
Kyle Wilson <kyle@ninecrows.com>
2018-02-27 22:09:41 -0500

    Added argument processing with one switch '-u' to show unused path items

--- C9Which/Program.cs
+++ C9Which/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,13 +12,22 @@ namespace C9Which
     {
         static void Main(string[] args)
         {
+            CommandLineApplication cla = new CommandLineApplication(false);
+            //CommandArgument items = null;
+            //cla.Command()
+
+            CommandOption showUnused = cla.Option("-u|--unused", "Show unused path elements", CommandOptionType.NoValue);
+            cla.HelpOption("-? | -h | --help");
+
             string path = Environment.GetEnvironmentVariable("path");
 
+            cla.Execute(args);
+            
             string[] targets = path.Split(';');
 
             if (args.Length > 0)
             {
-                string askAbout = args[0].ToLower();
+                string askAbout = /*args[0]*/cla.RemainingArguments[0].ToLower();
                 for (int ct = 0; ct < targets.Length; ct++)
                 {
                     string thisTarget = targets[ct];
@@ -37,7 +47,10 @@ namespace C9Which
                     }
                     else
                     {
-                        Console.Error.WriteLine($"Not a folder \"{thisTarget}\"");
+                        if (showUnused.HasValue())
+                        {
+                            Console.Error.WriteLine($"Not a folder \"{thisTarget}\"");
+                        }
                     }
                 }
             }
